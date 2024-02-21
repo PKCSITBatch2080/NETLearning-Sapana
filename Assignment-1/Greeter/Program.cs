12ee799d35ccedbeab30ddc57ab08cbe64364632
Sapana Shrestha <shresthasapana01@gmail.com>
2024-02-21 12:00:26 +0545

    added the final task

--- Assignment-1/Greeter/Program.cs
+++ Assignment-1/Greeter/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+Console.WriteLine("Enter your Name");
+string fullName = Console.ReadLine().ToUpper();
+
+Console.WriteLine("Enter your Date of Birth as 2010/12/12");
+DateTime DOB = DateTime.Parse(Console.ReadLine());
+
+DateTime today = DateTime.Today;
+
+TimeSpan age = today - DOB;
+int totalDays = Convert.ToInt32(age.TotalDays);
+int totalHours = Convert.ToInt32(age.TotalHours);
+int totalMinutes = Convert.ToInt32(age.TotalMinutes);
+
+int years = totalDays / 365;
+int months = (totalDays % 365) / 30;
+int weeks = (totalDays % 365 % 30) / 7;
+int days = totalDays % 365 % 30 % 7;
+
+Console.WriteLine($"Hello, {fullName} Ji");
+
+Console.WriteLine($"Your DOB is {DOB.ToString("dddd, dd MMMM yyyy")}.");
+
+Console.WriteLine($"You are {years} years {months} months {weeks} weeks and {days} days old. \n");
+
+// EXTRA
+
+Console.WriteLine($"You are {totalDays} days old");
+Console.WriteLine($"You are {totalHours} hours old");
+Console.WriteLine($"You are {totalMinutes} minutes old");
