e80941592580e249c90f6fab0aea769864bbb04b
Sapana Shrestha <shresthasapana01@gmail.com>
2024-02-26 08:04:36 +0545

    learn generic methods

---
+++ consoleapp/d.generics.cs
@@ -0,0 +1,11 @@
+// generic method ==> type as a argument
+using System;
+
+class Generic
+{
+    public T Display<T>(T value)
+    {
+        Console.WriteLine($"Value {value}");
+        return value;
+    }
+}
