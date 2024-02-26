c1e57019476f6f59d9b1f18d7987b7bad621125c
Sapana Shrestha <shresthasapana01@gmail.com>
2024-02-26 07:55:29 +0545

    more on methods

--- consoleapp/Program.cs
+++ consoleapp/Program.cs
@@ -6,3 +6,7 @@ dt1.Learn();
 
 Methods M=new();
 M.Greet("Namaste");
+var highest= M.GetMax(3.4,5.6,8.4,23.4);
+var highest2= M.GetMax(3.4,8.4,23.4);
+var highest3= M.GetMax(3.4,5.6);
+var highest4= M.GetMax(23.4);
--- consoleapp/c.method.cs
+++ consoleapp/c.method.cs
@@ -1,16 +1,12 @@
 using System;
+using System.Linq;
 
 class Methods
 {
-   public void Greet()
-    {
-        Console.WriteLine(" Hey, there!");
-    }
-    public void Greet(string greeting)
-    {
-        Console.WriteLine($" {greeting}, there!"); // string interpolation
+   public void Greet()=>Console.WriteLine(" Hey, there!"); //arrow function
+    public void Greet(string greeting) =>Console.WriteLine($" {greeting}, there!"); // string interpolation
 
-    }
+
 
     public float CalculateBMI(float weightInKg, float heightInfeet)
     {
@@ -18,5 +14,18 @@ class Methods
         var bmi= weightInKg/ (heightInM*heightInM);
         return bmi;
     }
+
+    public double GetMax(params double[] numbers)=> numbers.Max(); // max is built-in method, params keyword user garera arrary jasari pathauna parena, call garna easy vayo
+    // params accept variable number of argument
+
+    public (int,int) GetMinMax(params int[] numbers)
+    {
+         var smallest= numbers.Min();
+         var highest= numbers.Min();
+         return(smallest,highest); //tuple which will retun two intezer number
+
+         
+    }
+    
 
 }
