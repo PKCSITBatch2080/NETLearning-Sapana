fa803401f9c91eac4406dc6ff10a665afc11f818
Sapana Shrestha <shresthasapana01@gmail.com>
2024-02-26 07:33:37 +0545

    modified assignments

--- Assignment-1/Greeter/Program.cs
+++ Assignment-1/Greeter/Program.cs
@@ -10,8 +10,6 @@ DateTime today = DateTime.Today;
 
 TimeSpan age = today - DOB;
 int totalDays = Convert.ToInt32(age.TotalDays);
-int totalHours = Convert.ToInt32(age.TotalHours);
-int totalMinutes = Convert.ToInt32(age.TotalMinutes);
 
 int years = totalDays / 365;
 int months = (totalDays % 365) / 30;
@@ -24,8 +22,4 @@ Console.WriteLine($"Your DOB is {DOB.ToString("dddd, dd MMMM yyyy")}.");
 
 Console.WriteLine($"You are {years} years {months} months {weeks} weeks and {days} days old. \n");
 
-// EXTRA
 
-Console.WriteLine($"You are {totalDays} days old");
-Console.WriteLine($"You are {totalHours} hours old");
-Console.WriteLine($"You are {totalMinutes} minutes old");
