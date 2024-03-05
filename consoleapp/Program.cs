cd58a626e0b58f6513d87865d346683a34e9f857
Sapana Shrestha <shresthasapana01@gmail.com>
2024-03-05 12:18:58 +0545

    learn file directory

--- consoleapp/Program.cs
+++ consoleapp/Program.cs
@@ -1,29 +1,31 @@
 ﻿using System;
-Console.WriteLine("hiiii");
+// Console.WriteLine("hiiii");
 
-Datatypes dt1= new();
-dt1.Learn();
+// Datatypes dt1= new();
+// dt1.Learn();
 
-Methods M=new();
-M.Greet("Namaste");
-var highest= M.GetMax(3.4,5.6,8.4,23.4);
-var highest2= M.GetMax(3.4,8.4,23.4);
-var highest3= M.GetMax(3.4,5.6);
-var highest4= M.GetMax(23.4);
-//create instance of Generic class
-Generic g= new(); 
-g.Display <string> ("sapana shrestha");
-g.Display <int> (303);
+// Methods M=new();
+// M.Greet("Namaste");
+// var highest= M.GetMax(3.4,5.6,8.4,23.4);
+// var highest2= M.GetMax(3.4,8.4,23.4);
+// var highest3= M.GetMax(3.4,5.6);
+// var highest4= M.GetMax(23.4);
+// //create instance of Generic class
+// Generic g= new(); 
+// g.Display <string> ("sapana shrestha");
+// g.Display <int> (303);
 
-var Sapana= new Student();
-Student smriti= new Student("smriti kc",123);
-Sapana.RollNumber=5;
-//sapana.Dob=DateTime.Today; -> value of dob cannot be set 
+// var Sapana= new Student();
+// Student smriti= new Student("smriti kc",123);
+// Sapana.RollNumber=5;
+// //sapana.Dob=DateTime.Today; -> value of dob cannot be set 
 
-CSStudent rekha =new("Rekha Aryal",24,"Hospital Management System");
+// CSStudent rekha =new("Rekha Aryal",24,"Hospital Management System");
 
-var sapana =new CyberSecurityStudent("Sapana Shrestha",11, "HMS","RedHat"); //this is instance of CyberSecurity class
-sapana.StudentInfo();
+// var sapana =new CyberSecurityStudent("Sapana Shrestha",11, "HMS","RedHat"); //this is instance of CyberSecurity class
+// sapana.StudentInfo();
+
+FileDirectoryHandling.CreateFile(); //static member haru class ko hunxa object ko hunna
 
 
 
---
+++ consoleapp/g.fileDirectoryIO/fileDirectoryHandling.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+using System.Xml.Schema;
+
+class FileDirectoryHandling
+{
+    // File IO
+    public static void CreateFile()
+    {
+        var folderPath = @"D:\Dotnet-git\NETLearning-Sapana\consoleapp\g.fileDirectoryIO"; //absolute path
+        var filePath =$"{folderPath}\\student.txt";
+
+        File.WriteAllText(filePath,"This is first line of text");
+
+        Directory.CreateDirectory($"{folderPath}\\Dummy"); //folder banauxa
+
+        // create 10 files with names as File1.cs File2.cs.....
+        // each file should contain variable 'age' where value as 1,2,3....
+        // file bhitra -> byte age = 1;
+
+        for(int i=1; i<=10; i++)
+        {
+          var file =$"File{i}";
+          var fileP = $"{folderPath}\\Dummy\\{file}.cs";
+          File.WriteAllText(fileP,"class " +  file + "{ }");
+        }
+
+         // reading
+        string content = File.ReadAllText("D:\\Program.cs");
+        var words = content.Split([' ', ',', '.', ':', '-']);
+        Console.WriteLine(words.Length);
+
+    }
+}
