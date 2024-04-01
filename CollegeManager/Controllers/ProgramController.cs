8f64f7342dead7136ebcbe0d10646e08dda10514
Sapana Shrestha <shresthasapana01@gmail.com>
2024-04-01 07:54:36 +0545

    added packages

--- CollegeManager/Controllers/ProgramController.cs
+++ CollegeManager/Controllers/ProgramController.cs
@@ -2,23 +2,31 @@ using Microsoft.AspNetCore.Mvc;
 
 public class ProgramController : Controller
 {
+    List <CollegeProgram> programs = 
+    [
+        new() { Id = 1, Name = "CSIT", Affiliation="TU", StartedDate=DateTime.Now},
+        new() { Id = 2, Name = "BIM", Affiliation="PU", StartedDate=DateTime.Now.AddYears(-2)},
+        new(){ Id = 4, Name = "BIT", Affiliation="TU", StartedDate=DateTime.Now.AddDays(-200)},
+        new(){ Id = 4, Name = "BBA", Affiliation="TU", StartedDate=DateTime.Now.AddDays(-2000)},
+
+    ];
     public IActionResult Index()
-    { 
-        CollegeProgram c1 = new()
-         { Id = 1, Name = "CSIT", Affiliation="TU", StartedDate=DateTime.Now};
-
-        CollegeProgram c2 = new() 
-        { Id = 2, Name = "BIM", Affiliation="PU", StartedDate=DateTime.Now.AddYears(-2)};
-
-        CollegeProgram c3 = new() 
-        { Id = 3, Name = "BCA", Affiliation="TU", StartedDate=DateTime.Now.AddMonths(-11)};
-
-        CollegeProgram c4 = new() 
-        { Id = 4, Name = "BIT", Affiliation="TU", StartedDate=DateTime.Now.AddDays(-200)};
-
-        List <CollegeProgram> programs = new() { c1, c2, c3, c4 };
-        
+    {  
         return View(programs); // program ko data view lai pathaideko
-
     }
+
+    [HttpGet]
+    public IActionResult Add() 
+    {
+        return View(); 
+    }
+    [HttpPost] //C# attribute
+    public IActionResult Add(CollegeProgram program) //Model binding ->
+    //college program uta bata aauxa vanni inform gareko
+    {
+        //do something on program
+        programs.Add(program);
+        return RedirectToAction("Index"); //response code is 3xx
+    }
+
 }
